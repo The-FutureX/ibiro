7fcce4b202b3380b969f04ba0e7f77461a73b066
Surulere Chris. I. Abiye <suruabiye@gmail.com>
2023-02-28 11:16:18 +0100

    updates

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,4 +1,4 @@
-import React, {createContext, useContext} from 'react';
+import {createContext, useContext} from 'react';
 
 import {useAddress, useContract, useContractWrite, useMetamask} from '@thirdweb-dev/react';
 import {ethers} from 'ethers';
